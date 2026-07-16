95afac9d993e169d0050c3242bdd7dc7cdad82db
MIsha Pyshnyi <mishap635@gmail.com>
2026-07-16 21:13:28 +0300

    les3

---
+++ les3/index2.js
@@ -0,0 +1,61 @@
+let a ,d
+const b = 'Hello'
+var c 
+
+d = a + c 
+
+let e = "Hello"
+let f = 'Hello Misha!'
+let h = `Hello tildda`
+
+
+const name = `misha ${e}`
+const age = 31
+console.log(`hello  ${h} ${name} my age ${age}`)
+let number = 31.5
+let number1 = -3
+
+let result = "text" * 5
+console.log(result)
+
+console.log(10/0)
+
+
+let istrue = true 
+let isfalse = false 
+
+console.log(5>10)
+console.log(5<10)
+
+let username 
+console.log(username)
+username = "M"
+console.log(username)
+
+let userage = null
+console.log(userage)
+userage = 12
+console.log(userage)
+
+
+const a = 106787129847891427389471289342
+const b = 643124131312412412412
+console.log( a + BigInt(b))
+
+let id = Symbol("id")
+let id1 = Symbol("id")
+console.log(id === id1)
+
+let id2 = 2
+let id3 = 2
+console.log(id2 === id3)
+
+
+let a = "2"
+let b = 2
+let c = a + b
+console.log(c)
+
+const person = {
+    "name": "Misha"
+}
