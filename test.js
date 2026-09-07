a22b098325b7ff6f62b04bf294d1144fbd0de9fa
MIsha Pyshnyi <mishap635@gmail.com>
2026-09-07 19:55:12 +0300

    test pre-comit

---
+++ test.js
@@ -0,0 +1,7 @@
+const user = { name: 'Alex', age: 25 };
+
+const showUser = (user) => {
+  console.log(user.name, user.age);
+};
+
+showUser(user);
---
+++ test2.js
@@ -0,0 +1,7 @@
+const userName = 'Alex';
+const age = 25;
+const unusedVariable = 'test';
+
+if (age === '25') {
+  console.log('User found');
+}
